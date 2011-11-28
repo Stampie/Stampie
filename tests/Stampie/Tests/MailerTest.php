d6c1e30abe3068cecaae3ec7885911c5fe466fff
Henrik Bjørnskov <henrik@bjrnskov.dk>
2011-11-28 18:37:01 +0100

    first test for Mailer

--- tests/Stampie/Tests/MailerTest.php
+++ tests/Stampie/Tests/MailerTest.php
@@ -2,6 +2,33 @@
 
 namespace Stampie\Tests;
 
+use Stampie\Mailer;
+use Stampie\MessageInterface;
+
 class MailerTest extends \PHPUnit_Framework_TestCase
 {
+    const SERVER_TOKEN = "mySuperSecretServerToken";
+
+    public function setUp()
+    {
+        $this->buzz = $this->getMock('Buzz\Browser');
+    }
+
+    public function testSettersAndGetters()
+    {
+        $mailer = new Mailer($this->buzz);
+        $this->assertEquals($this->buzz, $mailer->getBrowser());
+        $this->assertEquals("POSTMARK_API_TEST", $mailer->getServerToken());
+
+        $mailer->setServerToken(static::SERVER_TOKEN, $mailer->getServerToken());
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testServerTokenCannotBeEmpty()
+    {
+        $mailer = new Mailer($this->buzz);
+        $mailer->setServerToken('');
+    }
 }
