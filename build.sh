40bda72d514169376ae1a6a21733fc4c20ad84cd
Henrik Bjørnskov <henrik@bjrnskov.dk>
2011-12-01 10:57:47 +0100

    Add autoloading directly to phar file. Change build.sh to include autoloading

--- build.sh
+++ build.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/env bash
 
 FILE="stub.php"
+OUTPUT="build/stampie.phar"
+SOURCE="lib"
+
 STUB="
 <?php
 Phar::mapPhar();
+
+spl_autoload_register(function (\$className) {
+    if (0 !== strpos(\$className, 'Stampie\\\')) {
+        die('adad');
+        return false;
+    }
+
+    \$file = 'phar://' . __FILE__ . '/' . str_replace('\\\', DIRECTORY_SEPARATOR, \$className) . '.php';
+
+    if (file_exists(\$file)) {
+        require \$file;
+        return true;
+    }
+
+    return false;
+});
 
 __HALT_COMPILER();
 "
 
 echo $STUB > $FILE
-phar-build --src=lib -ns --phar=build/stampie.phar --stub=$FILE
+phar-build --ns --src=$SOURCE --phar=$OUTPUT --stub=$FILE
 rm $FILE
