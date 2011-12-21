dfa9dc8877af6d12239e1b7cf3759168640526ec
Henrik Bjørnskov <henrik@bjrnskov.dk>
2011-12-21 09:40:25 +0100

    Fix error code

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,12 @@
 <?php
 
 if (!@include __DIR__ . '/../vendor/.composer/autoload.php') {
-    die(<<<'EOT'
+    echo <<<'EOT'
 You must set up the project dependencies, run the following commands:
 wget http://getcomposer.org/composer.phar
 php composer.phar install
-EOT
-    );
+EOT;
+
+    // Throw the correct error code, a build should fail if autoload isnt present.
+    exit(1);
 }
