e6a554658d9b9dbdd537b5a5aa56b77c424dfb96
Henrik Bjørnskov <henrik@bjrnskov.dk>
2012-05-21 09:38:11 +0200

    Sami configuration for building api documentation

---
+++ sami.php
@@ -0,0 +1,27 @@
+<?php
+
+use Sami\Sami;
+use Sami\Version\GitVersionCollection;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->exclude('Resources')
+    ->exclude('Tests')
+    ->in($dir = __DIR__ . '/lib')
+;
+
+// generate documentation for all v2.0.* tags, the 2.0 branch, and the master one
+$versions = GitVersionCollection::create($dir)
+    ->addFromTags('*')
+    ->add('master', 'master branch')
+;
+
+return new Sami($iterator, array(
+    'versions'             => $versions,
+    'title'                => 'Stampie API',
+    'build_dir'            => __DIR__.'/api/%version%',
+    'cache_dir'            => __DIR__.'/cache/%version%',
+    'default_opened_level' => 2,
+));
