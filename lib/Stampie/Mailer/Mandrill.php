14733c6d464a8a72747d6b8954edd3a212354f93
Christophe Coevoet <stof@notk.org>
2012-07-13 10:23:58 +0200

    Added a Mandrill mailer

---
+++ lib/Stampie/Mailer/Mandrill.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Stampie\Mailer;
+
+use Stampie\Mailer;
+use Stampie\MessageInterface;
+use Stampie\Adapter\ResponseInterface;
+use Stampie\Exception\HttpException;
+use Stampie\Exception\ApiException;
+
+/**
+ * Sends emails to Mandrill server
+ *
+ * @author Christophe Coevoet <stof@notk.org>
+ */
+class Mandrill extends Mailer
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getEndpoint()
+    {
+        return 'https://mandrillapp.com/api/1.0/messages/send.json';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getHeaders()
+    {
+        return array(
+            'Content-Type' => 'application/json',
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function format(MessageInterface $message)
+    {
+        $headers = array_filter(array_merge(
+            $message->getHeaders(),
+            array('Reply-To' => $message->getReplyTo())
+        ));
+        $parameters = array(
+            'key'     => $this->getServerToken(),
+            'message' => array_filter(array(
+                'from_email' => $message->getFrom(),
+                'to'         => array(array('email' => $message->getTo())),
+                'subject'    => $message->getSubject(),
+                'headers'    => $headers,
+                'text'       => $message->getText(),
+                'html'       => $message->getHtml(),
+            )),
+        );
+
+        return json_encode($parameters);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * "You can consider any non-200 HTTP response code an error - the returned data will contain more detailed information"
+     */
+    public function handle(ResponseInterface $response)
+    {
+        $httpException = new HttpException($response->getStatusCode(), $response->getStatusText());
+        $error         = json_decode($response->getContent());
+
+        throw new ApiException($error->message, $httpException, $error->code);
+    }
+}
---
+++ tests/Stampie/Tests/Mailer/MandrillTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Stampie\Tests\Mailer;
+
+use Stampie\Mailer\Mandrill;
+use Stampie\Adapter\Response;
+
+class MandrillTest extends \Stampie\Tests\BaseMailerTest
+{
+    const SERVER_TOKEN = '5daa75d9-8fad-4211-9b18-49124642732e';
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->mailer = new Mandrill(
+            $this->adapter,
+            self::SERVER_TOKEN
+        );
+    }
+
+    public function testEndpoint()
+    {
+        $this->assertEquals('https://mandrillapp.com/api/1.0/messages/send.json', $this->mailer->getEndpoint());
+    }
+
+    public function testHeaders()
+    {
+        $this->assertEquals(array(
+            'Content-Type' => 'application/json',
+        ), $this->mailer->getHeaders());
+    }
+
+    public function testFormat()
+    {
+        $message = $this->getMessageMock(
+            $from = 'hb@peytz.dk',
+            $to = 'henrik@bjrnskov.dk',
+            $subject = 'Stampie is awesome',
+            $html = 'So what do you thing'
+        );
+
+        $this->assertEquals(json_encode(array(
+            'key' => self::SERVER_TOKEN,
+            'message' => array(
+                'from_email' => $from,
+                'to' => array(array('email' => $to)),
+                'subject' => $subject,
+                'html' => $html,
+            ),
+        )), $this->mailer->format($message));
+    }
+
+    /**
+     * @dataProvider handleDataProvider
+     */
+    public function testHandle($statusCode, $content)
+    {
+        $response = new Response($statusCode, json_encode(array('message' => $content, 'code' => -1)));
+
+        try {
+            $this->mailer->handle($response);
+        } catch (\Stampie\Exception\ApiException $e) {
+            $this->assertInstanceOf('Stampie\Exception\HttpException', $e->getPrevious());
+            $this->assertEquals($e->getPrevious()->getMessage(), $content);
+            $this->assertEquals($e->getMessage(), $content);
+            return;
+        }
+
+        $this->fail('Expected Stampie\Exception\ApiException to be trown');
+    }
+
+    public function handleDataProvider()
+    {
+        return array(
+            array(400, 'Bad Request'),
+            array(401, 'Unauthorized'),
+            array(504, 'Gateway Timeout'),
+        );
+    }
+}
