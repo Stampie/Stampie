756e26791aeae3e56d63db61b8d5d87bb7ed9179
Henrik Bjørnskov <henrik@bjrnskov.dk>
2012-12-17 10:35:01 +0100

    fix sami config

--- sami.php
+++ sami.php
@@ -14,7 +14,7 @@ $iterator = Finder::create()
 
 // generate documentation for all v2.0.* tags, the 2.0 branch, and the master one
 $versions = GitVersionCollection::create($dir)
-    ->addFromTags('*')
+    ->addFromTags(array('0.6.0'))
     ->add('master', 'master branch')
 ;
 
