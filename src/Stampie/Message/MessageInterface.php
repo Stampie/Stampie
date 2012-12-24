be651c724fe198da9dce4a15d1bccb0f83600c32
Henrik Bjørnskov <henrik@bjrnskov.dk>
2012-12-24 14:58:46 +0100

    Add License header to all files

--- src/Stampie/Message/MessageInterface.php
+++ src/Stampie/Message/MessageInterface.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * (c) Henrik Bjornskov <henrik@bjrnskov.dk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Stampie\Message;
 
 use Stampie\Identity;
