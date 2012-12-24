7de34e977117f276486fca0c17f1f8e18396d4a8
Henrik Bjørnskov <henrik@bjrnskov.dk>
2012-12-24 14:35:07 +0100

    add spec for default prevented and dispatching event

---
+++ spec/Stampie/Mailer.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace spec\Stampie;
+
+use Mockery;
+use PHPSpec2\ObjectBehavior;
+use Stampie\Events;
+use Stampie\Identity;
+
+class Mailer extends ObjectBehavior
+{
+    /**
+     * @param Stampie\Handler\HandlerInterface $adapter
+     * @param Symfony\Component\EventDispatcher\EventDispatcher $dispatcher
+     * @param Stampie\Message\MessageInterface $message
+     */
+    function let($adapter, $dispatcher)
+    {
+        $this->beConstructedWith($adapter, $dispatcher);
+    }
+
+    /**
+     * @param Stampie\Event\MessageEvent $event
+     */
+    function it_dispatches_event_when_sending($dispatcher, $message, $event)
+    {
+        $dispatcher->dispatch(Events::SEND, Mockery::type('Stampie\Event\MessageEvent'))->shouldBeCalled()->willReturn($event);
+
+        $this->send(new Identity('henrik@bjrnskov.dk', 'Henrik Bjornskov'), $message);
+    }
+
+    /**
+     * @param Stampie\Event\MessageEvent $event
+     */
+    function it_disables_sending_when_default_is_prevented($dispatcher, $handler, $event, $message)
+    {
+        $dispatcher->dispatch(ANY_ARGUMENTS)->willReturn($event);
+
+        $event->isDefaultPrevented()->shouldBeCalled()->willReturn(true);
+
+        $handler->send(ANY_ARGUMENTS)->shouldNotBeCalled();
+
+        $this->send(new Identity('henrik@bjrnskov.dk', 'Henrik Bjornskov'), $message);
+    }
+}
