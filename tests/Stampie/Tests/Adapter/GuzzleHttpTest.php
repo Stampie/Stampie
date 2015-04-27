fcc82ff0c80cf9446539a7ff44edf067e16f21fd
Ninir <gauthier.wallet@gmail.com>
2015-04-27 10:23:29 +0200

    Added the GuzzleHttp adapter

---
+++ tests/Stampie/Tests/Adapter/GuzzleHttpTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Stampie\Tests\Adapter;
+
+use Stampie\Adapter\GuzzleHttp;
+
+class GuzzleHttpTest extends \PHPUnit_Framework_TestCase
+{
+    private $client;
+
+    public function setUp()
+    {
+        if (!interface_exists('GuzzleHttp\ClientInterface')) {
+            $this->markTestSkipped('Cannot find GuzzleHttp\ClientInterface');
+        }
+
+        $this->client = $this->getMock('GuzzleHttp\ClientInterface');
+    }
+
+    public function testAccessibility()
+    {
+        $adapter = new GuzzleHttp($this->client);
+        $this->assertEquals($this->client, $adapter->getClient());
+    }
+
+    public function testSend()
+    {
+        $adapter = new GuzzleHttp($this->client);
+        $response = $this->getResponseMock();
+        $request = $this->getRequestMock();
+        $client = $this->client;
+
+        $files = array(
+            'filename.jpg' => 'path/to/file.jpg',
+        );
+
+        $client
+            ->expects($this->once())
+            ->method('send')
+            ->will($this->returnValue($response))
+        ;
+
+        $response
+            ->expects($this->once())
+            ->method('getStatusCode')
+            ->will($this->returnValue(200))
+        ;
+
+        $request
+            ->expects($this->once())
+            ->method('setBody')
+        ;
+
+        $response
+            ->expects($this->once())
+            ->method('getBody')
+        ;
+
+        $client
+            ->expects($this->once())
+            ->method('createRequest')
+            ->with(
+                $this->equalTo('POST'),
+                $this->equalTo('http://google.com')
+            )
+            ->will(
+                $this->returnValue($request)
+            )
+        ;
+
+        $adapter->send('http://google.com', 'content', array(
+            'Content-Type' => 'application/json',
+            'X-Postmark-Server-Token' => 'MySuperToken',
+        ), $files);
+    }
+
+    protected function getRequestMock()
+    {
+        return $this->getMock('GuzzleHttp\Message\RequestInterface', array(), array(), '', null, true);
+    }
+
+    protected function getResponseMock()
+    {
+        return $this->getMock('GuzzleHttp\Message\Response', array(), array(), '', null, true);
+    }
+}
