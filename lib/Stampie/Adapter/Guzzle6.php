aa56d6195a3c0bebc25b02e54d77114debfe056d
Darren <darren@darrenmothersele.com>
2016-03-18 09:31:24 +0000

    code style fixes

--- lib/Stampie/Adapter/Guzzle6.php
+++ lib/Stampie/Adapter/Guzzle6.php
@@ -1,48 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: darren
- * Date: 18/03/2016
- * Time: 08:34
- */
 
 namespace Stampie\Adapter;
-
 
 use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Psr7\Request;
 
+/**
+ * GuzzleHttp Adapter (guzzlephp.org) - Version 6+
+ *
+ */
 class Guzzle6 implements AdapterInterface
 {
     /** @var ClientInterface $client */
-    protected $client;
+    private $client;
 
     /**
-     * Guzzle6 constructor.
-     * @param $client
+     * @param ClientInterface $client
      */
-    public function __construct($client)
+    public function __construct(ClientInterface $client)
     {
         $this->client = $client;
     }
 
+    /**
+     * @return ClientInterface
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
 
     function send($endpoint, $content, array $headers = array(), array $files = array())
     {
 
         $request = new Request('POST', $endpoint, $headers, $content);
 
-
         // TODO: handle files
-        //$body = new PostBody();
-        //
-        //if ($files && $request instanceof RequestInterface) {
-        //    foreach ($files as $name => $path) {
-        //        $body->addFile(new PostFile($name, fopen($path, 'r')));
-        //    }
-        //}
-        //
-        //$request->setBody($body);
         
         $response = $this->client->send($request);
 
