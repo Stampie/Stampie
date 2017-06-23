94a5c94cbf5f2ae2c80b97d0f48e76ae144c46f2
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-06-23 22:42:19 +0200

    Updated Saml config to list all versions (#76)
    * Updated sami config
    
    * Better filter versions
    
    * Tags from 0.8
    
    * specify tags

--- sami.php
+++ sami.php
@@ -3,6 +3,7 @@
 use Sami\Sami;
 use Sami\Version\GitVersionCollection;
 use Symfony\Component\Finder\Finder;
+use Sami\RemoteRepository\GitHubRemoteRepository;
 
 $iterator = Finder::create()
     ->files()
@@ -14,7 +15,7 @@ $iterator = Finder::create()
 
 // generate documentation for all v2.0.* tags, the 2.0 branch, and the master one
 $versions = GitVersionCollection::create($dir)
-    ->addFromTags('v0.6.0')
+    ->addFromTags(['0.8.0', '0.9.0', '0.10.0', '0.11.0'])
     ->add('master', 'master branch')
 ;
 
@@ -23,5 +24,6 @@ return new Sami($iterator, array(
     'title'                => 'Stampie API',
     'build_dir'            => __DIR__.'/api/%version%',
     'cache_dir'            => __DIR__.'/cache/%version%',
+    'remote_repository'    => new GitHubRemoteRepository('Stampie/Stampie', dirname($dir)),
     'default_opened_level' => 2,
 ));
