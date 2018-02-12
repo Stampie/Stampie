877a6e037c1c74e9709b5e648f108ed206f278ea
Tobias Nyholm <tobias.nyholm@gmail.com>
2018-02-12 09:37:19 +0100

    Apply fixes from StyleCI (#104)

--- sami.php
+++ sami.php
@@ -1,9 +1,9 @@
 <?php
 
+use Sami\RemoteRepository\GitHubRemoteRepository;
 use Sami\Sami;
 use Sami\Version\GitVersionCollection;
 use Symfony\Component\Finder\Finder;
-use Sami\RemoteRepository\GitHubRemoteRepository;
 
 $iterator = Finder::create()
     ->files()
@@ -15,8 +15,7 @@ $iterator = Finder::create()
 // generate documentation for all v2.0.* tags, the 2.0 branch, and the master one
 $versions = GitVersionCollection::create($dir)
     ->addFromTags(['0.8.0', '0.9.0', '0.10.0', '0.11.0'])
-    ->add('master', 'master branch')
-;
+    ->add('master', 'master branch');
 
 return new Sami($iterator, [
     'versions'             => $versions,
