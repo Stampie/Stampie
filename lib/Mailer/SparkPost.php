9a4bf64f693e0ece3c305e13bb11b0d6662fd240
Shaun <simshaun@gmail.com>
2018-02-12 03:35:42 -0500

    Add SparkPost mailer (#83)
    * Add SparkPost mailer
    
    * CS fixes
    
    * Bring mailer & test in line w/ changes on master
    
    * Make `transactional` configurable via mailer constructor argument
    
    * Trying to satisfy old PhpUnit 4.x on PHP 5.5
    
    * Document constructor
    
    * Simplify FQCN

---
+++ lib/Mailer/SparkPost.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace Stampie\Mailer;
+
+use Http\Client\HttpClient;
+use Psr\Http\Message\ResponseInterface;
+use Stampie\Attachment;
+use Stampie\Exception\ApiException;
+use Stampie\Exception\HttpException;
+use Stampie\Mailer;
+use Stampie\Message\AttachmentsAwareInterface;
+use Stampie\Message\MetadataAwareInterface;
+use Stampie\Message\TaggableInterface;
+use Stampie\MessageInterface;
+use Stampie\Util\IdentityUtils;
+
+/**
+ * Mailer to be used with SparkPost's API.
+ *
+ * @author Shaun Simmons <shaun@envysphere.com>
+ */
+class SparkPost extends Mailer
+{
+    private $transactional;
+
+    /**
+     * @param HttpClient $httpClient
+     * @param string     $serverToken
+     * @param bool       $transactional Whether messages are transactional for unsubscribe and suppression purposes
+     *
+     * @see https://en.wikipedia.org/wiki/Email_marketing#Transactional_emails
+     */
+    public function __construct(HttpClient $httpClient, $serverToken, $transactional = true)
+    {
+        parent::__construct($httpClient, $serverToken);
+        $this->transactional = (bool) $transactional;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getEndpoint()
+    {
+        return 'https://api.sparkpost.com/api/v1/transmissions';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getHeaders()
+    {
+        return [
+            'Content-Type' => 'application/json',
+            'Authorization' => $this->getServerToken(),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function format(MessageInterface $message)
+    {
+        $from = $this->normalizeIdentity($message->getFrom());
+
+        $parameters = [
+            'options' => [
+                'transactional' => $this->transactional,
+            ],
+            'content' => [
+                'from' => [
+                    'name' => $from->getName(),
+                    'email' => $from->getEmail(),
+                ],
+                'subject' => $message->getSubject(),
+                'text' => $message->getText(),
+                'html' => $message->getHtml(),
+            ],
+        ];
+
+        if ($message->getReplyTo()) {
+            $parameters['content']['reply_to'] = $message->getReplyTo();
+        }
+
+        if (count($message->getHeaders())) {
+            $parameters['content']['headers'] = $message->getHeaders();
+        }
+
+        if ($message instanceof AttachmentsAwareInterface) {
+            foreach ($message->getAttachments() as $attachment) {
+                $inline = $attachment->getId() !== null;
+                $parameters['content'][$inline ? 'inline_images' : 'attachments'][] = [
+                    'type' => $attachment->getType(),
+                    'name' => $inline ? $attachment->getId() : $attachment->getName(),
+                    'data' => base64_encode($this->getAttachmentContent($attachment)),
+                ];
+            }
+        }
+
+        if ($message instanceof MetadataAwareInterface) {
+            $metadata = array_filter($message->getMetadata());
+            if (count($metadata)) {
+                $parameters['metadata'] = $metadata;
+            }
+        }
+
+        $tags = [];
+        if ($message instanceof TaggableInterface) {
+            $tags = (array) $message->getTag();
+        }
+
+        $normalizedTo = $this->normalizeIdentities($message->getTo());
+        $normalizedCc = $this->normalizeIdentities($message->getCc());
+        $normalizedBcc = $this->normalizeIdentities($message->getBcc());
+        $toIdentityString = IdentityUtils::buildIdentityString($normalizedTo);
+
+        /** @var \Stampie\Identity[] $merged */
+        $merged = array_merge($normalizedTo, $normalizedCc, $normalizedBcc);
+        foreach ($merged as $recipient) {
+            /*
+             * The reason we are specifying header_to is so that each recipient can see *everybody* who received
+             * the email. Without it, each recipient will only see their email in the To field.
+             *
+             * IMPORTANT: Do not specify address.name!
+             * If given, SparkPost will try to wrap the entire header_to value, mangling it in the process.
+             */
+
+            $parameters['recipients'][] = [
+                'address' => [
+                    'email' => $recipient->getEmail(),
+                    'header_to' => $toIdentityString,
+                ],
+                'tags' => $tags,
+            ];
+        }
+
+        if (count($normalizedCc)) {
+            $parameters['content']['headers']['CC'] = IdentityUtils::buildIdentityString($normalizedCc);
+        }
+
+        return json_encode($parameters);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * HTTP codes 400-429 will throw an ApiException, otherwise an HttpException is thrown.
+     */
+    protected function handle(ResponseInterface $response)
+    {
+        $httpException = new HttpException($response->getStatusCode(), $response->getReasonPhrase());
+
+        // 400-429 will contain error information in the body encoded as JSON
+        if ($response->getStatusCode() >= 400 && $response->getStatusCode() <= 429) {
+            throw new ApiException((string) $response->getBody(), $httpException);
+        }
+
+        throw $httpException;
+    }
+
+    private function getAttachmentContent(Attachment $attachment)
+    {
+        return file_get_contents($attachment->getPath());
+    }
+}
---
+++ tests/Stampie/Tests/Mailer/SparkPostTest.php
@@ -0,0 +1,290 @@
+<?php
+
+namespace Stampie\Tests\Mailer;
+
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use Http\Client\HttpClient;
+use Stampie\Exception\ApiException;
+use Stampie\Exception\HttpException;
+use Stampie\Identity;
+use Stampie\Mailer\SparkPost;
+use Stampie\Tests\TestCase;
+
+class SparkPostTest extends TestCase
+{
+    const SERVER_TOKEN = '123abc';
+
+    /**
+     * @var SparkPost
+     */
+    private $mailer;
+
+    /**
+     * @var HttpClient|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $httpClient;
+
+    public function setUp()
+    {
+        $this->httpClient = $this->getMockBuilder(HttpClient::class)->getMock();
+        $this->mailer = new SparkPost($this->httpClient, self::SERVER_TOKEN);
+    }
+
+    public function testNonTransactional()
+    {
+        $mailer = new SparkPost($this->httpClient, self::SERVER_TOKEN, false);
+        $message = $this->getMessageMock('bob@example.com', 'alice@example.com', 'Stampie is awesome');
+
+        $this->httpClient
+            ->expects($this->once())
+            ->method('sendRequest')
+            ->with($this->callback(function (Request $request) {
+                $body = json_decode((string) $request->getBody(), true);
+                return $body['options']['transactional'] === false;
+            }))
+            ->willReturn(new Response());
+
+        $mailer->send($message);
+    }
+
+    public function testSend()
+    {
+        $message = $this->getMessageMock(
+            new Identity('bob@example.com', 'Bob'),
+            [new Identity('alice@example.com', 'Alice'), 'charlie@example.com'],
+            'Stampie is awesome!',
+            '<h1>Stampie</h1>',
+            'Stampie',
+            ['X-Custom-Header' => 'My Custom Header Value']
+        );
+
+        $message
+            ->expects($this->any())
+            ->method('getReplyTo')
+            ->will($this->returnValue('reply@example.com'));
+
+        $message
+            ->expects($this->any())
+            ->method('getCc')
+            ->will($this->returnValue([
+                new Identity('cc-mark@example.com', 'Mark'),
+                'cc-john@example.com',
+            ]));
+
+        $message
+            ->expects($this->any())
+            ->method('getBcc')
+            ->will($this->returnValue([new Identity('bcc-sally@example.com', 'Sally')]));
+
+        $this->httpClient
+            ->expects($this->once())
+            ->method('sendRequest')
+            ->with($this->callback(function (Request $request) {
+                $body = json_decode((string) $request->getBody(), true);
+                return
+                    $request->getMethod() === 'POST'
+                    && (string) $request->getUri() === 'https://api.sparkpost.com/api/v1/transmissions'
+                    && $request->getHeaderLine('Content-Type') === 'application/json'
+                    && $request->getHeaderLine('Authorization') === self::SERVER_TOKEN
+                    && $body == [
+                        'options' => ['transactional' => true],
+                        'content' => [
+                            'from' => ['email' => 'bob@example.com', 'name' => 'Bob'],
+                            'headers' => [
+                                'X-Custom-Header' => 'My Custom Header Value',
+                                'CC' => 'Mark <cc-mark@example.com>,cc-john@example.com',
+                            ],
+                            'subject' => 'Stampie is awesome!',
+                            'text' => 'Stampie',
+                            'html' => '<h1>Stampie</h1>',
+                            'reply_to' => 'reply@example.com',
+                        ],
+                        'recipients' => [
+                            [
+                                'address' => [
+                                    'email' => 'alice@example.com',
+                                    'header_to' => 'Alice <alice@example.com>,charlie@example.com',
+                                ],
+                                'tags' => [],
+                            ],
+                            [
+                                'address' => [
+                                    'email' => 'charlie@example.com',
+                                    'header_to' => 'Alice <alice@example.com>,charlie@example.com',
+                                ],
+                                'tags' => [],
+                            ],
+                            [
+                                'address' => [
+                                    'email' => 'cc-mark@example.com',
+                                    'header_to' => 'Alice <alice@example.com>,charlie@example.com',
+                                ],
+                                'tags' => [],
+                            ],
+                            [
+                                'address' => [
+                                    'email' => 'cc-john@example.com',
+                                    'header_to' => 'Alice <alice@example.com>,charlie@example.com',
+                                ],
+                                'tags' => [],
+                            ],
+                            [
+                                'address' => [
+                                    'email' => 'bcc-sally@example.com',
+                                    'header_to' => 'Alice <alice@example.com>,charlie@example.com',
+                                ],
+                                'tags' => [],
+                            ],
+                        ],
+                    ];
+            }))
+            ->willReturn(new Response());
+
+        $this->mailer->send($message);
+    }
+
+    public function testSendTaggable()
+    {
+        $message = $this->getTaggableMessageMock(
+            'bob@example.com',
+            'alice@example.com',
+            'Stampie is awesome!',
+            null,
+            null,
+            [],
+            ['foo', 'bar']
+        );
+
+        $this->httpClient
+            ->expects($this->once())
+            ->method('sendRequest')
+            ->with($this->callback(function (Request $request) {
+                $body = json_decode((string) $request->getBody(), true);
+
+                return array_key_exists('recipients', $body)
+                    && array_key_exists('tags', $body['recipients'][0])
+                    && $body['recipients'][0]['tags'] === ['foo', 'bar'];
+            }))
+            ->willReturn(new Response());
+
+        $this->mailer->send($message);
+    }
+
+    public function testSendMetadataAware()
+    {
+        $message = $this->getMetadataAwareMessageMock(
+            'bob@example.com',
+            'alice@example.com',
+            'Stampie is awesome',
+            null,
+            null,
+            [],
+            ['client_name' => 'Stampie']
+        );
+
+        $this->httpClient
+            ->expects($this->once())
+            ->method('sendRequest')
+            ->with($this->callback(function (Request $request) {
+                $body = json_decode((string) $request->getBody(), true);
+
+                return array_key_exists('metadata', $body) && $body['metadata'] === ['client_name' => 'Stampie'];
+            }))
+            ->willReturn(new Response());
+
+        $this->mailer->send($message);
+    }
+
+    public function testSendWithAttachments()
+    {
+        $message = $this->getAttachmentsMessageMock(
+            $from = null,
+            $to = null,
+            $subject = null,
+            $html = null,
+            $text = null,
+            $headers = [],
+            array_merge(
+                $attachments = [
+                    $this->getAttachmentMock('paper.txt', 'paper.txt', 'text/plain', null),
+                    $this->getAttachmentMock('apple.jpg', 'apple.jpg', 'image/jpeg', null),
+                ],
+                $images = [
+                    $this->getAttachmentMock('orange.jpg', 'orange.jpg', 'image/jpeg', 'orange'),
+                ]
+            )
+        );
+
+        $fixtureDir = __DIR__.'/../../../Fixtures';
+
+        $this->httpClient
+            ->expects($this->once())
+            ->method('sendRequest')
+            ->with($this->callback(function (Request $request) use ($fixtureDir) {
+                $body = json_decode((string) $request->getBody(), true);
+
+                return array_key_exists('inline_images', $body['content'])
+                    && $body['content']['inline_images'] === [
+                        [
+                            'type' => 'image/jpeg',
+                            'name' => 'orange',
+                            'data' => base64_encode(file_get_contents($fixtureDir.'/orange.jpg')),
+                        ]
+                    ]
+                    && array_key_exists('attachments', $body['content'])
+                    && $body['content']['attachments'] === [
+                        [
+                            'type' => 'text/plain',
+                            'name' => 'paper.txt',
+                            'data' => base64_encode(file_get_contents($fixtureDir.'/paper.txt')),
+                        ],
+                        [
+                            'type' => 'image/jpeg',
+                            'name' => 'apple.jpg',
+                            'data' => base64_encode(file_get_contents($fixtureDir.'/apple.jpg')),
+                        ],
+                    ];
+            }))
+            ->willReturn(new Response())
+        ;
+
+        $this->mailer->send($message);
+    }
+
+    /**
+     * @dataProvider badRequestProvider
+     */
+    public function testHandleBadRequest($httpStatusCode, $expectApiException)
+    {
+        // Hack to support old PhpUnit 4.x for PHP 5.5.
+        // @TODO remove when support for PHP 5.5 is dropped.
+        if (!method_exists($this, 'expectException')) {
+            $this->setExpectedException($expectApiException
+                ? '\Stampie\Exception\ApiException'
+                : '\Stampie\Exception\HttpException');
+        } else {
+            $this->expectException($expectApiException ? ApiException::class : HttpException::class);
+        }
+
+        $response = new Response($httpStatusCode);
+
+        $message = $this->getMessageMock('bob@example.com', 'alice@example.com', 'Stampie is awesome!');
+
+        $this->httpClient
+            ->method('sendRequest')
+            ->willReturn($response);
+
+        $this->mailer->send($message);
+    }
+
+    public function badRequestProvider() {
+        foreach ([400, 401, 403, 404, 405, 409, 415, 422, 429] as $code) {
+            yield [$code, true];
+        }
+
+        foreach ([500, 503] as $code) {
+            yield [$code, false];
+        }
+    }
+}
